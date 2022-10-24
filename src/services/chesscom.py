e2e4751614e991fd5e58ab514e982236ec463a7c
Rafal Jamro <r.jamro@gmail.com>
2022-10-24 22:41:24 +0200

    fixed chesscom service calls

--- src/services/chesscom.py
+++ src/services/chesscom.py
@@ -8,6 +8,9 @@ class ChessComApiService(object):
 
     async def get_games(self, player_name: str, year: int, month: int, session: ClientSession) -> list[str]:
         async with session.get(f'{self.chesscom_url}/pub/player/{player_name}/games/{year}/{month:02x}') as response:
+            if response.status != status.HTTP_200_OK:
+                raise HTTPException(status_code=response.status)
+
             decoded = await response.json()
             return [game['pgn'] for game in decoded['games']]
 
@@ -17,7 +20,7 @@ class ChessComApiService(object):
                 raise HTTPException(status_code=response.status)
 
             decoded = await response.json()
-            tactics = decoded['tactics']['highest']['rating'] if 'tactics' in decoded else None
+            tactics = decoded['tactics']['highest']['rating'] if 'tactics' in decoded and 'highest' in decoded['tactics'] else None
             rapid = decoded['chess_rapid']['last']['rating'] if 'chess_rapid' in decoded else None
             blitz = decoded['chess_blitz']['last']['rating'] if 'chess_blitz' in decoded else None
             bullet = decoded['chess_bullet']['last']['rating'] if 'chess_bullet' in decoded else None
